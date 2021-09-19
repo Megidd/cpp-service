2e119518d93de7ab83e7a4fdeb85507a8425856c
Megidd Git <megiddgit@gmail.com>
2021-09-19 15:04:57 +0430

    Service start handling POST request

--- test/server.cc
+++ test/server.cc
@@ -6,7 +6,21 @@ int main(void) {
   Server svr;
 
   svr.Get("/manu-supp", [](const Request & /*req*/, Response &res) {
-    res.set_content("Hello World!", "text/plain");
+    res.set_content("Creating manual supports...", "text/plain");
+  });
+
+  svr.Post("/manu-supp", [](const Request &req, Response &res) {
+    auto stl_file = req.get_file_value("stl_file");
+    
+    std::cout << "stl file length: " << stl_file.content.length() << std::endl
+         << "stl file name: " << stl_file.filename << std::endl;
+
+    {
+      std::ofstream ofs(stl_file.filename, std::ios::binary);
+      ofs << stl_file.content;
+    }
+
+    res.set_content("done", "text/plain");
   });
 
   int port = 58080;
