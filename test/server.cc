5f52051c3a140c965074ac4c9679a5ad79fc7d94
Megidd Git <megiddgit@gmail.com>
2021-09-19 15:42:24 +0430

    Test service provides a HTML form for:
    Easy testing with different inputs.

--- test/server.cc
+++ test/server.cc
@@ -2,22 +2,55 @@
 #include <iostream>
 using namespace httplib;
 
+const char *html = R"(
+<form id="formElem">
+  <div>
+    <label>STL file of 3D model</label>
+    <input type="file" name="stl_file" accept="stl/*">
+  </div>
+  <div>
+    <label>JSON file of placeholder coordinates</label>
+    <input type="file" name="json_file" accept="json/*">
+  </div>
+  <div>
+    <input type="submit">
+  </div>
+</form>
+<script>
+  formElem.onsubmit = async (e) => {
+    e.preventDefault();
+    let res = await fetch('/manu-supp', {
+      method: 'POST',
+      body: new FormData(formElem)
+    });
+    console.log(await res.text());
+  };
+</script>
+)";
+
 int main(void) {
   Server svr;
 
-  svr.Get("/manu-supp", [](const Request & /*req*/, Response &res) {
-    res.set_content("Creating manual supports...", "text/plain");
+  svr.Get("/", [](const Request & /*req*/, Response &res) {
+    res.set_content(html, "text/html");
   });
 
   svr.Post("/manu-supp", [](const Request &req, Response &res) {
     auto stl_file = req.get_file_value("stl_file");
-    
+    auto json_file = req.get_file_value("json_file");
+
     std::cout << "stl file length: " << stl_file.content.length() << std::endl
-         << "stl file name: " << stl_file.filename << std::endl;
+         << "stl file name: " << stl_file.filename << std::endl
+         << "json file length: " << json_file.content.length() << std::endl
+         << "json file name: " << json_file.filename << std::endl;
 
     {
       std::ofstream ofs(stl_file.filename, std::ios::binary);
       ofs << stl_file.content;
+    }
+    {
+      std::ofstream ofs(json_file.filename);
+      ofs << json_file.content;
     }
 
     res.set_content("done", "text/plain");
