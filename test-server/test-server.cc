eea1ae569730fcde3066094e27dbebb51c5b29ab
Megidd Git <megiddgit@gmail.com>
2021-09-20 17:16:13 +0430

    Format document

--- test-server/test-server.cc
+++ test-server/test-server.cc
@@ -28,31 +28,32 @@ const char *html = R"(
 </script>
 )";
 
-int main(void) {
+int main(void)
+{
   Server svr;
 
-  svr.Get("/", [](const Request & /*req*/, Response &res) {
-    res.set_content(html, "text/html");
-  });
+  svr.Get("/", [](const Request & /*req*/, Response &res)
+          { res.set_content(html, "text/html"); });
 
-  svr.Post("/manu-supp", [](const Request &req, Response &res) {
-    auto stl_file = req.get_file_value("stl_file");
-    auto json_file = req.get_file_value("json_file");
+  svr.Post("/manu-supp", [](const Request &req, Response &res)
+           {
+             auto stl_file = req.get_file_value("stl_file");
+             auto json_file = req.get_file_value("json_file");
 
-    std::cout << "stl file length: " << stl_file.content.length() << std::endl
-         << "stl file name: " << stl_file.filename << std::endl
-         << "json file length: " << json_file.content.length() << std::endl
-         << "json file name: " << json_file.filename << std::endl;
+             std::cout << "stl file length: " << stl_file.content.length() << std::endl
+                       << "stl file name: " << stl_file.filename << std::endl
+                       << "json file length: " << json_file.content.length() << std::endl
+                       << "json file name: " << json_file.filename << std::endl;
 
-    // Call the logic executable then wait for it to finish
-    // https://stackoverflow.com/a/5155626/3405291
-    // https://stackoverflow.com/a/31521217/3405291
-    char command[50];
-    std::sprintf (command, "../build/cpp-service -stl %s -json %s", stl_file.filename.c_str(), json_file.filename.c_str());
-    std::system(command);
+             // Call the logic executable then wait for it to finish
+             // https://stackoverflow.com/a/5155626/3405291
+             // https://stackoverflow.com/a/31521217/3405291
+             char command[50];
+             std::sprintf(command, "../build/cpp-service -stl %s -json %s", stl_file.filename.c_str(), json_file.filename.c_str());
+             std::system(command);
 
-    res.set_content("done", "text/plain");
-  });
+             res.set_content("done", "text/plain");
+           });
 
   int port = 58080;
   std::cout << "=> service going to listen on port: " << port << std::endl;
