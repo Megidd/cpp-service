8f38c87260ca89b376f797611d8309e86c1be5f4
Megidd Git <megiddgit@gmail.com>
2021-09-20 16:50:09 +0430

    Format document

--- main.cpp
+++ main.cpp
@@ -2,23 +2,30 @@
 #include <string>
 #include <vector>
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     std::cout << "Logic executable started!\n";
 
     // https://stackoverflow.com/a/442137/3405291
     std::vector<std::string> args(argv + 1, argv + argc);
     std::string pathStl, pathJson;
 
-    for (auto i = args.begin(); i != args.end(); ++i) {
-        if (*i == "-h" || *i == "--help") {
+    for (auto i = args.begin(); i != args.end(); ++i)
+    {
+        if (*i == "-h" || *i == "--help")
+        {
             std::cout << "Syntax: cpp-service -stl <stl_file> -json <json_file>" << std::endl;
             return 0;
-        } else if (*i == "-stl") {
+        }
+        else if (*i == "-stl")
+        {
             pathStl = *++i;
-        } else if (*i == "-json") {
+        }
+        else if (*i == "-json")
+        {
             pathJson = *++i;
         }
     }
 
-    std::cout << "STL=="<< pathStl << " | JSON==" << pathJson;
+    std::cout << "STL==" << pathStl << " | JSON==" << pathJson;
 }
