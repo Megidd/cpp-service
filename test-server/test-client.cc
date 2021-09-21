5e27278567645f9c47e7000660c803c76c4e8995
Megidd Git <megiddgit@gmail.com>
2021-09-21 10:05:42 +0430

    Change test:
    * Add client
    * Avoid HTML form: they cannot submit absolute input file path

---
+++ test-server/test-client.cc
@@ -0,0 +1,24 @@
+#include "httplib.h"
+#include <iostream>
+
+int main(void)
+{
+    httplib::Client cli("localhost", 58080);
+
+    httplib::Params params{
+        {"stl", "~/repos/cpp-service/test-server/input/input.stl"},
+        {"json", "~/repos/cpp-service/test-server/input/placeholders.json"}};
+
+    if (auto res = cli.Post("/manu-supp", params))
+    {
+        std::cout << res->status << std::endl;
+        std::cout << res->get_header_value("Content-Type") << std::endl;
+        std::cout << res->body << std::endl;
+    }
+    else
+    {
+        std::cout << "error code: " << res.error() << std::endl;
+    }
+
+    return 0;
+}
--- test-server/test-server.cc
+++ test-server/test-server.cc
@@ -2,57 +2,33 @@
 #include <iostream>
 using namespace httplib;
 
-const char *html = R"(
-<form id="formElem">
-  <div>
-    <label>STL file of 3D model containing mesh:</label>
-    <input type="file" name="stl_file" accept="stl/*">
-  </div>
-  <div>
-    <label>JSON file of placeholder coordinates:</label>
-    <input type="file" name="json_file" accept="json/*">
-  </div>
-  <div>
-    <input type="submit">
-  </div>
-</form>
-<script>
-  formElem.onsubmit = async (e) => {
-    e.preventDefault();
-    let res = await fetch('/manu-supp', {
-      method: 'POST',
-      body: new FormData(formElem)
-    });
-    console.log(await res.text());
-  };
-</script>
-)";
-
 int main(void)
 {
   Server svr;
 
-  svr.Get("/", [](const Request & /*req*/, Response &res)
-          { res.set_content(html, "text/html"); });
-
   svr.Post("/manu-supp", [](const Request &req, Response &res)
            {
-             auto stl_file = req.get_file_value("stl_file");
-             auto json_file = req.get_file_value("json_file");
+             if (!req.has_param("stl") || !req.has_param("json"))
+             {
+               res.set_content("stl and json paths must be provided", "text/plain");
+             }
+             else
+             {
+               auto stl = req.get_param_value("stl");
+               auto json = req.get_param_value("json");
 
-             std::cout << "stl file length: " << stl_file.content.length() << std::endl
-                       << "stl file name: " << stl_file.filename << std::endl
-                       << "json file length: " << json_file.content.length() << std::endl
-                       << "json file name: " << json_file.filename << std::endl;
+               std::cout << "stl: " << stl << std::endl
+                         << "json: " << json << std::endl;
 
-             // Call the logic executable then wait for it to finish
-             // https://stackoverflow.com/a/5155626/3405291
-             // https://stackoverflow.com/a/31521217/3405291
-             char command[50];
-             std::sprintf(command, "../build/cpp-service -stl %s -json %s", stl_file.filename.c_str(), json_file.filename.c_str());
-             std::system(command);
+               // Call the logic executable then wait for it to finish
+               // https://stackoverflow.com/a/5155626/3405291
+               // https://stackoverflow.com/a/31521217/3405291
+               char command[50];
+               std::sprintf(command, "../build/cpp-service -stl %s -json %s", stl.c_str(), json.c_str());
+               std::system(command);
 
-             res.set_content("done", "text/plain");
+               res.set_content("done", "text/plain");
+             }
            });
 
   int port = 58080;
