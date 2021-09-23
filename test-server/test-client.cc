a47a2d377c085c1fd6dabee6a14ea7ce91f2fe7a
Megidd Git <megiddgit@gmail.com>
2021-09-23 09:47:08 +0330

    Change STL input for validation test

--- test-server/test-client.cc
+++ test-server/test-client.cc
@@ -6,7 +6,7 @@ int main(void)
     httplib::Client cli("localhost", 58080);
 
     httplib::Params params{
-        {"stl", "~/repos/cpp-service/test-server/input/input.stl"},
+        {"stl", "~/repos/cpp-service/test-server/input/teapot_10elev.stl"},
         {"json", "~/repos/cpp-service/test-server/input/placeholders.json"}};
 
     if (auto res = cli.Post("/manu-supp", params))
